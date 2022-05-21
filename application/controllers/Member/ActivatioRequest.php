9aaa91ee6e9b17f56d5c365e71b740bf382f2db4
NitaiKOIRI463 <55233409+iotasonl@users.noreply.github.com>
2022-05-21 19:22:43 +0530

    bhfbv

--- application/controllers/Member/ActivatioRequest.php
+++ application/controllers/Member/ActivatioRequest.php
@@ -10,8 +10,37 @@
 
 		public function index()
 		{
+
+			$api = 'Plan/getPlanList';
+			$data = '';
+			$method = 'POST';
+			$result = $this->CallAPI($api, $data, $method);
+			$d['package_list'] = $result['data'];
+
+			$api = 'Package/get_pending_packages';
+			$data = 'member_id='.$this->session->userdata('user_id');
+			$method = 'POST';
+			$result = $this->CallAPI($api, $data, $method);
+			$d['prevData'] = $result['data'];
+
+			$api = 'Package/get_bitcoin_address';
+			$data = '';
+			$method = 'POST';
+			$result = $this->CallAPI($api, $data, $method);
+			$d['bitcoin_address'] = $result['data'];
+
+
 			$d['v'] = "Member/memerActivation_request";
 			$this->load->view('templates',$d);
+		}
+
+		public function purchasePackage()
+		{
+			$api = 'Package/BuyPackage';
+			$data = 'package_id='.$this->input->post('package_id',true).'&member_id='.$this->session->userdata('user_id');
+			$method = 'POST';
+			$result = $this->CallAPI($api, $data, $method);
+			echo json_encode($result,true);
 		}	
 	}
 ?>
